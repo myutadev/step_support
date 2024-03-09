f4dc9255688546f35331e6e14361b4430539dbc7
myuta <myuta.dev@gmail.com>
2024-03-09 20:21:21 +0800

    add logout function for admin and fix headers for admin

---
+++ app/Http/Controllers/Auth/Admin/AuthenticatedSessionController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Admin;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthenticatedSessionController extends Controller
+{
+
+    public function create(): View
+    {
+        return view('admin.auth.register');
+    }
+
+    // 登録実行
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'last_name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . Admin::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $admin = Admin::create([
+            'last_name' => $request->last_name,
+            'first_name' => $request->first_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($admin));
+
+        Auth::guard('admin')->login($admin);
+
+        return redirect()->route('admin.dashboard')->with([
+            'login_msg' => 'ログインしました。',
+        ]);
+    }
+
+    // ログイン画面呼び出し
+    public function showLoginPage(): View
+    {
+        return view('admin.auth.login');
+    }
+
+    // ログイン実行
+    public function login(LoginRequest $request): RedirectResponse
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (Auth::guard('admin')->attempt($credentials)) {
+            return redirect()->route('admin.daily')->with([
+                'login_msg' => 'ログインしました。',
+            ]);
+        }
+
+        return back()->withErrors([
+            'login' => ['ログインに失敗しました'],
+        ]);
+    }
+
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }
+}
