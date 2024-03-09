3ec5266eb835b126b1b6294c7ca14ebdf7315eaf
myuta <myuta.dev@gmail.com>
2024-03-09 18:39:35 +0800

    fix redirectMethod for Request with admin route

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,13 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        // return $request->expectsJson() ? null : route('login');
+        if (!$request->expectsJson()) {
+            //パスがadmin/から始まる場合、管理者ログイン画面へリダイレクト
+            if ($request->is('admin/*')) {
+                return route('admin.login');
+            }
+            return route('login');
+        }
     }
 }
